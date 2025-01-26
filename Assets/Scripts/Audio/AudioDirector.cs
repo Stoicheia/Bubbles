12c47d44d8490bc1977e50d4b4b409911c6bd9d9
Stoicheia <37011850+Stoicheia@users.noreply.github.com>
2025-01-26 23:03:58 +1100

    .

---
+++ Assets/Scripts/Audio/AudioDirector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Audio
+{
+    public class AudioDirector : MonoBehaviour
+    {
+        public static AudioDirector Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(this);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Bubbles/Game Panels/Automater/SceneAssociator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+
+namespace Bubbles.GamePanels.Automater
+{
+    [CreateAssetMenu(fileName = "Soccer", menuName = "Soccer", order = 0)]
+    public class SceneAssociator : ScriptableObject
+    {
+        [SerializeField] private string _suffix;
+        [SerializeField] private string _folderWithScenes;
+        [SerializeField] private string _folderWithScriptables;
+
+        [Button]
+        public void CreateScenes()
+        {
+            Dictionary<string, SceneDefinition> sceneDefs = new Dictionary<string, SceneDefinition>();
+            string[] files = Directory.GetFiles(_folderWithScriptables, "*.asset", SearchOption.TopDirectoryOnly);
+            foreach (string file in files)
+            {
+                SceneDefinition sceneDef = AssetDatabase.LoadAssetAtPath<SceneDefinition>(file);
+                string defName = sceneDef.name;
+                string soccerName = defName.Replace(_suffix, "");
+                sceneDefs.Add(soccerName, sceneDef);
+            }
+            
+            List<string> scenes = Directory.GetFiles(_folderWithScenes, "*.prefab", SearchOption.TopDirectoryOnly).ToList();
+            foreach (var kvp in sceneDefs)
+            {
+                string sceneName = kvp.Key;
+                if (!scenes.Contains(sceneName))
+                {
+                    Debug.LogWarning($"No associated scene found for scriptable {sceneName}.");
+                    continue;
+                }
+
+                SceneDefinition def = sceneDefs[sceneName];
+                
+            }
+        }
+    }
+}
